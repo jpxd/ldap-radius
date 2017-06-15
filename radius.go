b8eca3ad17bf7f860cc5bc952b891695c0f6ea83
Colin Shea <colin@evaryont.me>
2017-06-15 01:13:30 -0700

    replace the radius packet logging to avoid leaking passwords

--- radius.go
+++ radius.go
@@ -11,25 +11,25 @@ type radiusService struct{}
 var radiusServer *radius.Server
 
 func (p radiusService) RadiusHandle(request *radius.Packet) *radius.Packet {
-	log.Printf("[auth] %s\n", request.String())
+	log.Printf("[auth] New connection, %s for user %s\n", request.Code.String(), request.GetAVP(radius.UserName))
 	npac := request.Reply()
 	switch request.Code {
 	case radius.AccessRequest:
 		if checkCredentials(request.GetUsername(), request.GetPassword()) {
 			npac.Code = radius.AccessAccept
-			log.Printf("[auth] Is OK\n"))
+			log.Printf("[auth] Is OK\n")
 			return npac
 		}
-		log.Printf("[auth] Is incorrect, Go away!\n"))
+		log.Printf("[auth] Is incorrect, Go away!\n")
 		npac.Code = radius.AccessReject
 		npac.AVPs = append(npac.AVPs, radius.AVP{Type: radius.ReplyMessage, Value: []byte("Go away!")})
 		return npac
 	case radius.AccountingRequest:
-		log.Printf("[acct] Accounting request, sending response\n"))
+		log.Printf("[acct] Accounting request, sending response\n")
 		npac.Code = radius.AccountingResponse
 		return npac
 	default:
-		log.Printf("[radius] Access rejected.\n"))
+		log.Printf("[radius] Access rejected.\n")
 		npac.Code = radius.AccessReject
 		return npac
 	}
